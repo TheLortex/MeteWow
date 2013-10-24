949b7ac9d6db7ccfe3fc791e687d4e9f705e2961
Lucas <lucas.pluvinage@gmail.com>
2013-10-24 15:12:36 -0400

    Je sais plus

--- web/tile.js
+++ web/tile.js
@@ -64,8 +64,11 @@ $(document).ready(function() {
     $("body").on("click","#tileset article div button", function() {
         var p = $(this).parent();
         var sh = p.next();
-        if(!sh.is("div"))
+        var viewGraph = true;
+        if(!sh.is("div")) {
             sh = p.prev();
+            viewGraph = false;
+        }
         
         p.transition({
           rotateY: '90deg'
@@ -77,6 +80,64 @@ $(document).ready(function() {
             
             sh.transition({
               rotateY: '0deg'
+            }, function() {
+                	sh.first().highcharts('StockChart', {
+                		chart : {
+                			events : {
+                				load : function() {
+                
+                					// set up the updating of the chart each second
+                					var series = this.series[0];
+                					setInterval(function() {
+                						var x = (new Date()).getTime(), // current time
+                						y = Math.round(Math.random() * 100);
+                						series.addPoint([x, y], true, true);
+                					}, 1000);
+                				}
+                			}
+                		},
+                		
+                		rangeSelector: {
+                			buttons: [{
+                				count: 1,
+                				type: 'minute',
+                				text: '1M'
+                			}, {
+                				count: 5,
+                				type: 'minute',
+                				text: '5M'
+                			}, {
+                				type: 'all',
+                				text: 'All'
+                			}],
+                			inputEnabled: false,
+                			selected: 0
+                		},
+                		
+                		title : {
+                			text : 'Live random data'
+                		},
+                		
+                		exporting: {
+                			enabled: false
+                		},
+                		
+                		series : [{
+                			name : 'Random data',
+                			data : (function() {
+                				// generate an array of random data
+                				var data = [], time = (new Date()).getTime(), i;
+                
+                				for( i = -999; i <= 0; i++) {
+                					data.push([
+                						time + i * 1000,
+                						Math.round(Math.random() * 100)
+                					]);
+                				}
+                				return data;
+                			})()
+                		}]
+                	});
             });
         });
     });
