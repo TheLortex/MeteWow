3ff239dd3c0a410b8ce091cfbed98b355edf9030
Lucas <lucas.pluvinage@gmail.com>
2013-10-28 19:09:12 -0400

    Ajout des structures de bdd

--- api/datamanager.php
+++ api/datamanager.php
@@ -3,13 +3,29 @@
 class DataManager {
     private $database;
     
-    public function registerServer($mac); // return server id;
-    public function registerSensor($server_id, $display_name, $display_unit, $type); // return Sensor object;
-    public function addData($server_id, $sensor_id, $value, $time = now());
+    function __construct() {
+        loadDatabase();
+    }
+    
+    public function registerServer($mac,$secret); // return server id;
+    public function registerSensor($server_id, $secret, $display_name, $display_unit, $type); // return Sensor object;
+    public function addData($server_id, $secret, $sensor_id, $value, $time = now());
     
     public function getServers();  // return array of id of registered servers
     public function getSensors($server_id); // return array of Sensor object;
     public function getData($server_id, $sensor_id, $from, $to = now()) // return array of pair<time, value>
-    // Je vise le 42 jours de commit streak alors yolo.
+    
+    private function loadDatabase() {
+        try {
+            $database = new PDO("sqlite:db.sqlite");
+            $database->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        
+            $database->exec("CREATE TABLE IF NOT EXISTS mtw_servers (id INTEGER PRIMARY KEY, mac VARCHAR(255) UNIQUE, secret VARCHAR(255))");
+            $database->exec("CREATE TABLE IF NOT EXISTS mtw_sensors (id INTEGER PRIMARY KEY, mtw_server_id INTEGER, name VARCHAR(255), unit VARCHAR(255), category VARCHAR(4))");
+            $database->exec("CREATE TABLE IF NOT EXISTS mtw_data    (id INTEGER PRIMARY KEY, mtw_sensor_id INTEGER, value DOUBLE, time TIMESTAMP DEFAULT CURRENT_TIMESTAMP())");
+        } catch (Exception $e) {
+           die('Erreur : '.$e->getMessage());
+        }
+    }
 }
 ?>
