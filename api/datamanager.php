cd52738cadf3a2d85791201b33b6534025745108
Lucas <lucas.pluvinage@gmail.com>
2013-10-29 07:24:14 -0400

    Implémentation des setters

--- api/datamanager.php
+++ api/datamanager.php
@@ -1,19 +1,40 @@
 <?php
 
-class DataManager {
+abstract class DataManager {
     private $database;
     
     function __construct() {
         loadDatabase();
     }
     
-    public function registerServer($mac,$secret); // return server id;
-    public function registerSensor($server_id, $secret, $display_name, $display_unit, $type); // return Sensor object;
-    public function addData($server_id, $secret, $sensor_id, $value, $time = now());
+    public function registerServer($mac,$secret) {
+        $req = $database->prepare("INSERT INTO mtw_servers (mac,secret) VALUES (?,?)");
+        $req->execute(array($mac,$secret));
+        
+        return $database->lastInsertId();
+    }
     
-    public function getServers();  // return array of id of registered servers
-    public function getSensors($server_id); // return array of Sensor object;
-    public function getData($server_id, $sensor_id, $from, $to = now()) // return array of pair<time, value>
+    public function registerSensor($server_id, $secret, $display_name, $display_unit, $type) {
+        if(!auth_server($server_id, $secret))
+            die();
+            
+        $req = $database->prepare("INSERT INTO mtw_sensors (mtw_server_id,name,unit,category) VALUES (?,?,?,?)");
+        $req->execute(array($server_id,$display_name,$display_unit,$type));
+        
+        //TODO: Create sensor object
+    }
+    public function addData($server_id, $secret, $sensor_id, $value, $time) {
+        if(!auth_server($server_id, $secret))
+            die();
+            
+        $req = $database->prepare("INSERT INTO mtw_data (mtw_sensor_id,value,time) VALUES (?,?,?)");
+        $req->execute(array($sensor_id,$value,$time));
+    }
+    
+    public abstract function getServers();  // return array of id of registered servers
+    public abstract function getSensors($server_id); // return array of Sensor object;
+    public abstract function getData($server_id, $sensor_id, $from, $to); // return array of pair<time, value>
+    
     
     private function loadDatabase() {
         try {
@@ -26,6 +47,10 @@ class DataManager {
         } catch (Exception $e) {
            die('Erreur : '.$e->getMessage());
         }
+    }
+    
+    private function auth_server($server_id, $secret) {
+        return true;
     }
 }
 ?>
